a048e570219e177c369ed8569575995d7f2667d3
Tokimikichika <ahileses@mail.ru>
2023-12-24 22:54:39 +0300

    добавил условия проверки в sendStreams

--- src/screens/Room.jsx
+++ src/screens/Room.jsx
@@ -41,7 +41,14 @@ const RoomPage = () => {
 
   const sendStreams = useCallback(() => {
     for (const track of myStream.getTracks()) {
-      peer.peer.addTrack(track, myStream);
+        if (!addedTracks.has(track.id) && !peer.peer.getSenders().some(sender => sender.track === track)) {
+            console.log('Adding track:', track);
+            const sender = peer.peer.addTrack(track, myStream);
+            console.log('Sender added:', sender);
+            setAddedTracks(new Set([...addedTracks, track.id]));
+        } else {
+            console.log('Skipped track:', track);
+        }
     }
   }, [myStream]);
 
