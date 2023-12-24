0c950f6237ba0d20634366d48a3c55eba2ae0ffa
Tokimikichika <ahileses@mail.ru>
2023-12-24 21:35:45 +0300

    add part 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
 import { SocketProvider } from './context/SocketProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,4 +19,5 @@ root.render(
   </React.StrictMode>
 );
 
+reportWebVitals();
 
